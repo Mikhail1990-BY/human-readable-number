87371654430da9995b8db41701abfb944b9d7ec6
Mikhail Kaladzianets <68966313+Mikhail1990-BY@users.noreply.github.com>
2020-10-19 22:53:56 +0300

    feat: add solution

--- src/index.js
+++ src/index.js
@@ -1,3 +1,12 @@
 module.exports = function toReadable (number) {
-  
+  let numbersArr = ['', 'one', 'two', 'three', 'four', 'five', 'six',
+   'seven', 'eight', 'nine', 'ten', 'eleven', 'twelve', 'thirteen',
+    'fourteen', 'fifteen','sixteen', 'seventeen', 'eighteen', 'nineteen', 'twenty'];
+  let tensArr = ['', '', 'twenty', 'thirty', 'forty', 'fifty', 'sixty', 'seventy',
+        'eighty', 'ninety'];
+  if (number === 0) {return 'zero'};
+   if (number < 20) {return numbersArr[number]};
+   if (number >= 20 && number < 100) {return (`${tensArr[Math.trunc(number / 10)]} ${numbersArr[number % 10]}`).trim()}; 
+   if (number >= 100) { return (`${numbersArr[Math.trunc(number / 100)]} hundred ${((number % 100) >= 21 ? tensArr[Math.trunc((number % 100) / 10)] +
+     ' ' +  numbersArr[(number % 100) % 10] : numbersArr[number % 100])}`).trim()};
 }
